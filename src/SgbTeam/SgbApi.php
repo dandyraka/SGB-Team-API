cadbb1051d78848d78940bb24ff1455a920947b8
Dandy Raka <dandyraka13@gmail.com>
2019-10-24 22:44:37 +0700

    Add Region function

--- src/SgbTeam/SgbApi.php
+++ src/SgbTeam/SgbApi.php
@@ -8,6 +8,7 @@ class SgbApi
     public $Code;
     public $Key;
     public $SecretKey;
+    Public $Region;
 
     public function setCode()
     {
@@ -24,6 +25,11 @@ class SgbApi
         return $this->SecretKey;
     }
     
+    public function setRegion()
+    {
+        return $this->Region;
+    }
+    
     function Connect()
     {
         $headers = ['Referer' => 'https://' . $_SERVER['HTTP_HOST']];
@@ -35,6 +41,26 @@ class SgbApi
             array(
                 'form_params' => array(
                     'code' => $this->Code,
+                    'key' => $this->Key
+                )
+            )
+        );
+        $response = $send->getBody()->getContents();
+        return $response;
+    }
+    
+    function Region()
+    {
+        $headers = ['Referer' => 'https://' . $_SERVER['HTTP_HOST']];
+        $client = new Client([
+            'headers' => $headers
+        ]);
+        $send = $client->post(
+            'https://api.sgbteam.id/absenReg.php',
+            array(
+                'form_params' => array(
+                    'code' => $this->Code,
+                    'region' => $this->Region,
                     'key' => $this->Key
                 )
             )
